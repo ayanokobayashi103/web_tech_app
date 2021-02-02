9c0409186ba17db1e6da84850500270391cc4153
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-02-02 15:37:35 +0900

    graphql first commit

--- Gemfile
+++ Gemfile
@@ -35,3 +35,5 @@ group :test do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'graphql', '<= 1.12.3'
+gem 'graphiql-rails', '<= 1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
   root "blogs#index"
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+#   post '/graphql', to: 'graphql#execute'
 end
